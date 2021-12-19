1796fc4d02b164127d937d5e39dfa053fd3dfdbf
Ryosuke-Androiddev <Ryosuke0819NR@gmail.com>
2021-12-19 14:21:15 +0900

    change bar color to black and modify background color

--- app/src/main/java/com/example/tumotanapp/feature/presentation/MainActivity.kt
+++ app/src/main/java/com/example/tumotanapp/feature/presentation/MainActivity.kt
@@ -3,10 +3,12 @@ package com.example.tumotanapp.feature.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import com.example.tumotanapp.feature.presentation.navigation.NavigationFun
@@ -20,7 +22,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             TumotanAppTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
+                Surface(
+                    color = MaterialTheme.colors.background,
+                    modifier = Modifier.fillMaxSize()
+                ) {
 
                     val navController = rememberNavController()
 
